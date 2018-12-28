45e7fca3a58dec6474462f39a7da6a46a5c51061
zhenzhen <@2868382033.com>
2018-12-28 20:06:01 +0800

    起服务成功

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,6 @@ gulp.task("watch", function() {
 gulp.task('webserver', function() {
     gulp.src('src')
         .pipe(server({
-            open: true,
             port: 9090,
             middleware: function(req, res, next) {
                 var pathname = url.parse(req.url).pathname;
@@ -31,11 +30,11 @@ gulp.task('webserver', function() {
                     return
                 } else if (pathname == '/api/list') {
                     let { pagenum, limit } = url.parse(req.url, true).query;
-                    let totle = Math.ceil(datas.length / limit);
-                    var start = (pagenum - 1) * limit;
-                    var end = pagenum * limit;
+                    var totle = Math.ceil(datas.length / limit);
+                    var start = (pagenum - 1) * limit,
+                        end = pagenum * limit;
                     var target = datas.slice(start, end);
-                    res.end(JSON.stringify({ code: 0, data: target, totle }))
+                    res.end(JSON.stringify({ code: 0, data: target, totle: totle }))
 
                 } else {
                     pathname = pathname === '/' ? 'index.html' : pathname;
--- src/js/page/index.js
+++ src/js/page/index.js
@@ -6,37 +6,74 @@ require(["./js/config.js"], function() {
 
         function init() {
             ajax();
-            bscroll();
-        }
 
-        function bscroll() {
-            var scroll = new BScroll(".wrapper-bscroll")
         }
 
         function ajax() {
             $.ajax({
                 url: "/api/list",
-                type: "get",
                 dataType: "json",
+                type: "get",
                 data: {
                     pagenum: pagenum,
                     limit: limit,
-                    totle: totle
+                    totle
                 },
                 success: function(res) {
                     if (res.code == 0) {
-                        totle = res.totle;
-                        renderList(res.data)
+                        totle = res.totle
+                        renderList(res.data);
+
                     }
                 }
             })
         }
 
         function renderList(data) {
+            var str = "";
             data.forEach((i) => {
-
+                str += `<dl>
+                            <dt><img src="${i.img}" alt=""></dt>
+                            <dd>
+                                <b>${i.title}</b>
+                                <p><span>门店</span><span>中国香港片</span></p>
+                                <p><i>新品</i><i>公益宝贝</i></p>
+                                <p><b>${i.money}</b><b>4342付款</b></p>
+                            </dd>
+                        </dl>`;
             })
+            $(".section-bscroll-main").append(str);
+            scroll.refresh()
         }
+
+
+
+        var scroll = new BScroll(".wrapper-bscroll", {
+            probeType: 2
+        })
+
+        scroll.on("scroll", function(position) {
+            if (position.y < this.maxScrollY - 30) {
+                if (pagenum < totle) {
+                    $(".section-bscroll-main").attr("title", "释放加载更多")
+                } else {
+                    $(".section-bscroll-main").attr("title", "没有更多数据")
+                }
+            } else if (position.y < this.maxScrollY - 15) {
+                if (pagenum < totle) {
+                    $(".section-bscroll-main").attr("title", "上拉加载")
+                }
+            }
+        })
+
+        scroll.on("touchEnd", function() {
+            if ($(".section-bscroll-main").attr("title") == "释放加载更多") {
+                if (pagenum < totle) {
+                    pagenum++;
+                    ajax()
+                }
+            }
+        })
 
         init();
     })
